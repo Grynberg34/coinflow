9260cc1851f26cf756932496a9c71af4b517473e
FranciscoGB <franciscogrynberg34@gmail.com>
2021-02-09 15:10:32 -0300

    v0.2.1 versão mobile do menu de admin de empresa

--- public/scripts/admin.js
+++ public/scripts/admin.js
@@ -6,14 +6,18 @@ for(var i=0; i< option.length; i++){
 }
 
 function hoverOn(){
-  this.style.border = "3px solid #2bffd5";
-  this.classList.add('pulse');
-  this.firstElementChild.classList.add('verde-claro');
-  this.lastElementChild.classList.add('verde-claro');
+    if (screen.width > 767) {
+        this.style.border = "3px solid #2bffd5";
+        this.classList.add('pulse');
+        this.firstElementChild.classList.add('verde-claro');
+        this.lastElementChild.classList.add('verde-claro');
+    }
 }
 function hoverOff(){
-    this.style.border = "3px solid #4472ff";
-    this.classList.remove('pulse');
-    this.firstElementChild.classList.remove('verde-claro');
-    this.lastElementChild.classList.remove('verde-claro');
+    if (screen.width > 767) {
+        this.style.border = "3px solid #4472ff";
+        this.classList.remove('pulse');
+        this.firstElementChild.classList.remove('verde-claro');
+        this.lastElementChild.classList.remove('verde-claro');
+    }
 }
