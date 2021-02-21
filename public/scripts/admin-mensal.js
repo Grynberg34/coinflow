1bce25a0c21ae8c04dbe315d8bdec85c8f9a10b1
FranciscoGB <franciscogrynberg34@gmail.com>
2021-02-20 21:20:48 -0300

    v0.2.8 correção de datas para formato brasileiro na página de 'envios mensais' e 'histórico de envios'

--- public/scripts/admin-mensal.js
+++ public/scripts/admin-mensal.js
@@ -10,9 +10,65 @@ function submitForm() {
 
 function formatarData() {
 
-    var data = document.getElementsByClassName('mensal__date__timestamp')[0];
-    let str = data.textContent.substr(4, 20);
+    var data = document.getElementsByClassName('mensal__date__timestamp')[0]
+
+    let dia = data.textContent.substr(7,4);
+    let anohora = data.textContent.substr(10, 11);
+    let mes_ingles = data.textContent.substr(4, 3);
+    
+
+    if (mes_ingles == 'Jan') {
+        var mes = 'Jan'
+    }
+
+    if (mes_ingles == 'Feb') {
+        var mes = 'Fev'
+    }
+
+    if (mes_ingles == 'Mar') {
+        var mes = 'Mar'
+    }
+
+    if (mes_ingles == 'Apr') {
+        var mes = 'Abr'
+    }
+
+    if (mes_ingles == 'May') {
+        var mes = 'Mai'
+    }
+
+    if (mes_ingles == 'Jun') {
+        var mes = 'Jun'
+    }
+
+    if (mes_ingles == 'Jul') {
+        var mes = 'Jul'
+    }
+
+    if (mes_ingles == 'Aug') {
+        var mes = 'Ago'
+    }
+
+    if (mes_ingles == 'Sep') {
+        var mes = 'Set'
+    }
+
+    if (mes_ingles == 'Oct') {
+        var mes = 'Out'
+    }
+
+    if (mes_ingles == 'Nov') {
+        var mes = 'Nov'
+    }
+
+    if (mes_ingles == 'Dec') {
+        var mes = 'Dez'
+    }
+
+    let str = dia + mes +anohora;
+
     data.textContent = str;
+
 }
 
 formatarData()
