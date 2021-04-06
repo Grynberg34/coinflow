2d75ae3c4afeade19e635e91000afd246abd2c47
FranciscoGB <franciscogrynberg34@gmail.com>
2021-04-06 17:26:05 -0300

    v0.4.1 adicionada área de texto na funcionalidade de envio de outflow

--- routes/user.js
+++ routes/user.js
@@ -58,6 +58,7 @@ router.post('/:id/enviar', function(req, res) {
   var outflow = req.user.outflow;
   var user = req.body.user;
   var value = req.body.value;
+  var motivo = req.body.motivo;
   var notfound = 'Usuário não encontrado.';
   var insuficiente = 'Saldo insuficiente.';
   var saldo = 'Outflow enviado com sucesso.';
@@ -68,7 +69,7 @@ router.post('/:id/enviar', function(req, res) {
       else if (user != user.id && value < outflow && user_id == id) {
         connection.query(`UPDATE funcionarios SET outflow = flowin + '${value}' WHERE id_empresa = '${id_empresa}' and id = '${user}'`)
         connection.query(`UPDATE funcionarios SET outflow = outflow - '${value}' WHERE id_empresa = '${id_empresa}' and id = '${user_id}'`)
-        connection.query(`INSERT INTO transferencias (id_empresa, data, id_remetente, id_destinatario, valor) VALUES ('${id_empresa}', NOW(), '${user_id}', '${user}', '${value}')`)
+        connection.query(`INSERT INTO transferencias (id_empresa, data, id_remetente, id_destinatario, valor, motivo) VALUES ('${id_empresa}', NOW(), '${user_id}', '${user}', '${value}', '${motivo}')`)
         connection.query(`SELECT * FROM funcionarios WHERE id_empresa = '${id_empresa}' AND id = '${user_id}'`, function(err, new_user, fields) {
           if (err) res.render('error')
           var new_outflow = new_user[0].outflow
